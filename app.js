71ff5f28f366ed521f6560487d8aeb159eb6a96b
ashwin-srao <ashwin.r@tataelxsi.co.in>
2022-11-19 06:41:36 +0530

    Modified to port 8080

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express= require("express");
 const app = express();
 const bodyparser = require('body-parser')
 //const mysql = require("./DbConnect").connection;
-const port = process.env.port || process.env.PORT || 3001;
+const port = process.env.port || process.env.PORT || 8080;
 
 app.use(bodyparser.urlencoded({extended: false}))
 app.use(bodyparser.json())
