cd353960c35a2104a2f6f2fb638bc85299c5f311
harsh.k <harsh.k@tataelxsi.co.in>
2022-12-19 14:41:05 +0530

    API Changes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express= require("express");
+const fs = require('fs');
 const app = express();
 const bodyparser = require('body-parser')
 const mysql = require("./DbConnect").connection;
@@ -78,75 +79,84 @@ app.use("/changePassword",(req, res) => {
 
 //api for menu
 app.post("/dashboard",(req, res) => {
-    let userId  = req.param('userId',null);
-    let qry = "select * from user where userId = '" + userId + "'";
-    mysql.query(qry, (err, results) => {
-        if (err)
-            res.status(500).json({response:"500",status:'Error',message:"Internal Server Error"});
-        else {
-            if (results.length>0) {
-                qry1="select userId,userName from user where userId ='" + userId + "'";
-                mysql.query(qry1, (err, result) => {
-                    //res.status(200).json({response:"200",status:'success',message:"login Successful",user:result});
-                    if(result.length>0){
-                    qry2 = "SELECT role.roleId ,role.roleName from user , role, user_role where user.userId = user_role.userId and role.roleId = user_role.roleId and user.userId = '" + userId + "'" ;
-                    mysql.query(qry2, (err, result2) => {
-                        if (err)
-                        res.status(500).json({response:"500",status:'Error',message:"Internal Server Error"});   
-                        else {
-                        if (result2.length>0) {
-                        qry3 = "select m.menuId,m.menuName from menu m join role_menu rm on rm.menuId = m.menuId;"
-                        mysql.query(qry3, (err, result3) => {
-                            if (err)
-                        res.status(500).json({response:"500",status:'Error',message:"Internal Server Error"});   
-                        else {
-                        if (result3.length>0) {
-                            res.status(200).json({response:"200",status:'success',message:"login Successful",data:{userdata:result,roledata:result2,menudata:result3}});
-                        }
-                    }
-                        })
-                    }
-                    }})
-            }})
-            }else{
-                res.status(404).json({response:"404",status:'Failed',message:"Invalid Credential"});
-            }
-        }
-    });
+    let rawdata = fs.readFileSync('menu.json');
+    let menuData = JSON.parse(rawdata);
+    res.status(200).json({response:"200",status:'success',data:menuData});
+    // let userId  = req.param('userId',null);
+    // let qry = "select * from user where userId = '" + userId + "'";
+    // mysql.query(qry, (err, results) => {
+    //     if (err)
+    //         res.status(500).json({response:"500",status:'Error',message:"Internal Server Error"});
+    //     else {
+    //         if (results.length>0) {
+    //             qry1="select userId,userName from user where userId ='" + userId + "'";
+    //             mysql.query(qry1, (err, result) => {
+    //                 //res.status(200).json({response:"200",status:'success',message:"login Successful",user:result});
+    //                 if(result.length>0){
+    //                 qry2 = "SELECT role.roleId ,role.roleName from user , role, user_role where user.userId = user_role.userId and role.roleId = user_role.roleId and user.userId = '" + userId + "'" ;
+    //                 mysql.query(qry2, (err, result2) => {
+    //                     if (err)
+    //                     res.status(500).json({response:"500",status:'Error',message:"Internal Server Error"});   
+    //                     else {
+    //                     if (result2.length>0) {
+    //                     qry3 = "select m.menuId,m.menuName from menu m join role_menu rm on rm.menuId = m.menuId;"
+    //                     mysql.query(qry3, (err, result3) => {
+    //                         if (err)
+    //                     res.status(500).json({response:"500",status:'Error',message:"Internal Server Error"});   
+    //                     else {
+    //                     if (result3.length>0) {
+    //                         res.status(200).json({response:"200",status:'success',message:"login Successful",data:{userdata:result,roledata:result2,menudata:result3}});
+    //                     }
+    //                 }
+    //                     })
+    //                 }
+    //                 }})
+    //         }})
+    //         }else{
+    //             res.status(404).json({response:"404",status:'Failed',message:"Invalid Credential"});
+    //         }
+    //     }
+    // });
 });
 //user_details api
 
 app.use("/userDetails",allowOnlyPost,(req, res) => {
-    let userId  = req.param('userId',null);
-    qry = "SELECT user.username ,role.roleName from user , role, user_role where user.userId = user_role.userId and role.roleId = user_role.roleId and user.userId = '" + userId + "'" ;
+    let rawdata = fs.readFileSync('role.json');
+    let userDetails = JSON.parse(rawdata);
+    res.status(200).json({response:"200",status:'success',data:userDetails});
+    // let userId  = req.param('userId',null);
+    // qry = "SELECT user.username ,role.roleName from user , role, user_role where user.userId = user_role.userId and role.roleId = user_role.roleId and user.userId = '" + userId + "'" ;
 
-    mysql.query(qry, (err, results) => {
-        if (err)
-            res.status(500).json({response:"500",status:'Error',message:"Internal Server Error"});
-        else {
-            if (results.length > 0) {
-                res.status(200).json({response:"200",status:'success',data:results});
-            }else{
-                res.status(404).json({response:"404",status:'Failed',message:"Invalid Credential"});
-            }
-        }
-    });
+    // mysql.query(qry, (err, results) => {
+    //     if (err)
+    //         res.status(500).json({response:"500",status:'Error',message:"Internal Server Error"});
+    //     else {
+    //         if (results.length > 0) {
+    //             res.status(200).json({response:"200",status:'success',data:results});
+    //         }else{
+    //             res.status(404).json({response:"404",status:'Failed',message:"Invalid Credential"});
+    //         }
+    //     }
+    // });
 });
 
 //packageInfo API
 app.use("/packageInfo", (req, res) => {
-    let qry = "select * from package_info "; 
-    mysql.query(qry, (err, results) => { 
-    if (err)
-        res.status(500).json({response:"500",status:'Error',message:"Internal Server Error"});
-    else {
-            if (results.length > 0) {
-                 res.status(200).json({response:"200",status:'success',data:results});
-            }else{
-                 res.status(404).json({response:"404",status:'Failed',message:"Invalid Credential"});
-            }
-         }
-    })
+    let rawdata = fs.readFileSync('packageInfo.json');
+    let packageInfo = JSON.parse(rawdata);
+    res.status(200).json({response:"200",status:'success',data:packageInfo});
+    // let qry = "select * from package_info "; 
+    // mysql.query(qry, (err, results) => { 
+    // if (err)
+    //     res.status(500).json({response:"500",status:'Error',message:"Internal Server Error"});
+    // else {
+    //         if (results.length > 0) {
+    //              res.status(200).json({response:"200",status:'success',data:results});
+    //         }else{
+    //              res.status(404).json({response:"404",status:'Failed',message:"Invalid Credential"});
+    //         }
+    //      }
+    // })
      });
 
 
